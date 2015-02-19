9befd18e11880cb86dae4aaacbaa2d9a1b956e44
srausser <srausser@gmail.com>
2015-02-18 22:31:18 -0800

    made pj1 file

---
+++ projects/prj1/cubicRootFinder.m
@@ -0,0 +1,8 @@
+function [ output_args ] = cubicRootFinder( input_args )
+%project 1 
+%   Want to find roots of cubic polynomial
+%   https://math.berkeley.edu/~mgu/MA128A2015S/Prog1.pdf
+
+
+end
+
