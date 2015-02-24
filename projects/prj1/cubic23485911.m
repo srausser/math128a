146c2cbbc3622c31233f5d40c37f3733fddf0855
Sam Rausser <srausser@sr-macbookair.local>
2015-02-23 21:59:08 -0800

    switch computers

--- projects/prj1/cubic23485911.m
+++ projects/prj1/cubic23485911.m
@@ -1,7 +1,8 @@
-function [ output_args ] = cubicRootFinder( input_args )
+function [rts,info] = cubic23485911(C)
 %project 1 
 %   Want to find roots of cubic polynomial
 %   https://math.berkeley.edu/~mgu/MA128A2015S/Prog1.pdf
+
 
 
 end
